68324ff066ca9046a867c9bc23b739dd52b2b0bc
muritador5050 <muritador5050@gmail.com>
2026-02-23 11:18:34 +0100

    feat:added a preflight to the cors origin

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -58,6 +58,7 @@ export const createServer = (): Express => {
         maxAge: 86400,
       }),
     )
+    .options('*', cors())
     .use(express.json({ limit: '1mb' }))
     .use(express.urlencoded({ extended: true, limit: '1mb' }))
 
