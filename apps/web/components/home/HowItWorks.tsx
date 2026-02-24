28564c1b929689822f9897526aa1b1c99fcd2124
muritador5050 <muritador5050@gmail.com>
2026-02-24 21:04:36 +0100

    change padding on mobile

--- apps/web/components/home/HowItWorks.tsx
+++ apps/web/components/home/HowItWorks.tsx
@@ -72,7 +72,7 @@ export const HowItWorks = () => {
       bg='surface'
       mb={{ base: 48, md: 32 }}
       borderRadius={{ base: 'none', md: '4xl' }}
-      p={{ base: 6, md: 10 }}
+      p={{ base: 2, md: 10 }}
       pb={{ base: '150px', md: '200px' }}
       position='relative'
     >
